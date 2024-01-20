6f8fa98fbcdad2107bdde3f124273c0fe49bb062
Victor Payno <vpayno@users.noreply.github.com>
2024-01-19 23:53:09 -0800

    example: album

---
+++ albums-merge.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/questions/19529688/how-to-merge-2-json-objects-from-2-files-using-jq
+
+# top-level: object ->
+#	reduce .[] as $item ({}; . * $item)
+#	. * input
+# top-level: list -> flatten | group_by(keys[]) | map(reduce .[] as $item ({}; . * $item))
+
+# order here matters, user data goes after cue export
+{
+	cue export --out json ./album/*.cue
+	jq . ./album/user.json
+} | jq --slurp 'reduce .[] as $item ({}; . * $item)'
